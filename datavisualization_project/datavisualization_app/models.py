79aafeeb94e546a4e2fb0e9fd5d6379d606a759c
Hattah10 <norhata.sampaco@g.msuiit.edu.ph>
2023-11-16 21:51:20 +0800

    initial commit

--- datavisualization_project/datavisualization_app/models.py
+++ datavisualization_project/datavisualization_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Dengue(models.Model):  
+    id = models.AutoField(primary_key=True)
+    Location = models.CharField(max_length=100)  
+    Cases = models.PositiveIntegerField() 
+    Deaths = models.PositiveIntegerField()
+    Date = models.DateField()
+    Region = models.CharField(max_length=100) 
+   
+    class Meta:  
+        db_table = "dengue_dataset"
--- datavisualization_project/datavisualization_app/views.py
+++ datavisualization_project/datavisualization_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from datavisualization_app.models import Dengue
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    labels = []
+    data = []
+
+    queryset = Dengue.objects.all()[:5]
+    for dataset in queryset:
+        labels.append(dataset.Location)
+        data.append(dataset.Cases)
+
+
+    return render(request, 'index.html', {'labels':labels, 'data':data})
 
